732b662a67efee5fb1d6e5885f86cd955fe758fb
Lisette <l.mukiza@alustudent.com>
2026-03-15 23:57:37 +0400

    Create 0-minoperations.py

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+
+def minOperations(n):
+    """Return the minimum number of operations to reach n H characters"""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
